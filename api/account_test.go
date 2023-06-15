e6962ae1fe8519c8a3945e0b70c19679c691ed51
Riadul Islam <130457171+riadul-arisaftech@users.noreply.github.com>
2023-06-15 10:19:18 +0600

    update

---
+++ api/account_test.go
@@ -0,0 +1,61 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	mockdb "riad.com/simple_bank/db/mock"
+
+	"github.com/golang/mock/gomock"
+	db "riad.com/simple_bank/db/sqlc"
+	"riad.com/simple_bank/util"
+)
+
+func TestGetAccountAPI(t *testing.T) {
+	account := randomAccount()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	store := mockdb.NewMockStore(ctrl)
+	store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account.ID)).Times(1).Return(account, nil)
+
+	server := NewServer(store)
+	recorder := httptest.NewRecorder()
+
+	uri := fmt.Sprintf("/accounts/%d", account.ID)
+	request, err := http.NewRequest(http.MethodGet, uri, nil)
+	require.NoError(t, err)
+
+	server.router.ServeHTTP(recorder, request)
+
+	require.Equal(t, http.StatusOK, recorder.Code)
+
+	requireBodyMatchAccount(t, recorder.Body, account)
+}
+
+func randomAccount() db.Account {
+	return db.Account{
+		ID:       util.RandomInt(1, 1000),
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+}
+
+func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer, account db.Account) {
+	data, err := ioutil.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotAccount db.Account
+	err = json.Unmarshal(data, &gotAccount)
+	require.NoError(t, err)
+	require.Equal(t, account, gotAccount)
+}
